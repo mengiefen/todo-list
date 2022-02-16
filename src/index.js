7d3ac48172db271436b86109903dd4f204eb6564
mengiefen <mengiefen@gmail.com>
2022-02-16 11:46:50 +0300

    add more event listeners

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
-import todoList from './modules/todo-list.js';
-import './style.css';
-import appendTodoList from './modules/appendToDoList.js';
+import todoList from "./modules/todo-list.js";
+import "./style.css";
+import appendTodoList from "./modules/appendToDoList.js";
+import TODO from "./modules/todoList.js";
+import { descInput, clearCompleted, statusInput } from "./modules/elements.js";
 
 todoList.forEach((task) => {
   appendTodoList(task);
+});
+
+const newTodo = new TODO();
+
+descInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter" && descInput.value !== "") {
+    newTodo.addTodo();
+  }
+});
+
+clearCompleted.addEventListener("click", () => {
+  newTodo.clearCompleted();
+});
+
+statusInput.forEach((input) => {
+  input.addEventListener("change", (event) => {
+    const parentNode = event.taget.parentNode;
+    const 
+  });
 });
