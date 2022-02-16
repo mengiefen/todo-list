e3166d8854696cffc539ccebcca1606333f7530c
mengiefen <mengiefen@gmail.com>
2022-02-16 11:43:20 +0300

    add querySelector for checkboxes

---
+++ src/modules/elements.js
@@ -0,0 +1,5 @@
+const descInput = document.querySelector('#todo-input');
+const clearCompleted = document.querySelector('.btn-clear');
+const statusInput = document.querySelector('.status');
+
+export { descInput, clearCompleted, statusInput };
