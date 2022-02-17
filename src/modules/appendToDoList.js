91d588e2e40c8fef06f7e6a482f8a3a782311944
mengiefen <mengiefen@gmail.com>
2022-02-17 20:06:33 +0300

    modify the file

--- src/modules/appendToDoList.js
+++ src/modules/appendToDoList.js
@@ -1,30 +1,91 @@
 import { todoList } from './elements.js';
 
 const createTodoListRow = (todoItem) => {
-  const rowHTML = `
-  <div class="todo-left">
-    <input type="checkbox" class="status" id = ${todoItem.index}  ${
-  todoItem.completed ? 'checked' : ''
-} />
-    <p class="description ${todoItem.completed ? 'strike-through' : ''}">${
-  todoItem.description
-}</p>
-  </div>
-  <button class="btn btn-edit">
-    <i class="bi bi-three-dots-vertical"></i>
-  </button>  
-  <button class="btn btn-delete">  
-    <i class="bi bi-trash"></i>
-  </button>`;
-  return rowHTML;
+  const rowLeft = document.createElement('div');
+  const todoInputCheckBox = document.createElement('input');
+  const todoPara = document.createElement('p');
+  const btnEdit = document.createElement('button');
+  const iconThreeDots = document.createElement('i');
+  const btnDelete = document.createElement('button');
+  const iconTrash = document.createElement('i');
+  const rowElements = document.createElement('div');
+  const rowHolder = document.createElement('div');
+
+  const names = [
+    rowLeft,
+    todoInputCheckBox,
+    todoPara,
+    btnEdit,
+    iconThreeDots,
+    btnDelete,
+    iconTrash,
+    rowElements,
+    rowHolder,
+  ];
+
+  const classNames = [
+    'todo-left',
+    'status',
+    'description',
+    'btn',
+    'bi',
+    'btn',
+    'bi',
+    'row-elements',
+    'todo-list-row',
+  ];
+
+  names.forEach((name, index) => {
+    name.classList.add(classNames[index]);
+  });
+
+  btnEdit.classList.add('btn-edit');
+  btnDelete.classList.add('btn-delete');
+  iconThreeDots.classList.add('bi-three-dots-vertical');
+  iconTrash.classList.add('bi-trash');
+  if (todoItem.completed) {
+    todoPara.classList.add('strike-through');
+  } else {
+    todoPara.classList.remove('strike-through');
+  }
+
+  todoPara.innerText = todoItem.description;
+
+  todoInputCheckBox.setAttribute('type', 'checkbox');
+  todoInputCheckBox.setAttribute('id', `input-${todoItem.index}`);
+  if (todoItem.completed) {
+    todoInputCheckBox.checked = true;
+  } else {
+    todoInputCheckBox.checked = false;
+  }
+
+  rowLeft.appendChild(todoInputCheckBox);
+  rowLeft.appendChild(todoPara);
+
+  btnEdit.appendChild(iconThreeDots);
+  btnDelete.appendChild(iconTrash);
+
+  rowElements.appendChild(rowLeft);
+  rowElements.appendChild(btnEdit);
+  rowElements.appendChild(btnDelete);
+  rowElements.setAttribute('id', todoItem.index);
+  rowElements.draggable = true;
+  rowHolder.appendChild(rowElements);
+  rowHolder.setAttribute('data-id', todoItem.index);
+  return rowHolder;
 };
 
 const appendTodoList = (todoItem) => {
-  const todoListRow = document.createElement('div');
-  todoListRow.classList.add('todo-list-row');
-  const rowHTML = createTodoListRow(todoItem);
-  todoListRow.innerHTML = rowHTML;
+  const todoListRow = createTodoListRow(todoItem);
   todoList.appendChild(todoListRow);
 };
 
-export default appendTodoList;
+const removeChildNodes = () => {
+  let child = todoList.firstChild;
+  while (child) {
+    todoList.removeChild(child);
+    child = todoList.firstChild;
+  }
+};
+
+export { removeChildNodes, appendTodoList };
